69acebb8dcfa17facd1e42cbaa2955e64656b7c5
mrndolo <callistomutunga@gmail.com>
2024-09-28 16:05:22 -0400

    created new app and view function

---
+++ products/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('New page inside Products i.e. -> /products/new/')
