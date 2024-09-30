8fc37f101d23a6c0b6426f7ef82a2e3769fa066f
mrndolo <callistomutunga@gmail.com>
2024-09-29 21:48:13 -0400

    customizing the admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Product, ProductAdmin)
